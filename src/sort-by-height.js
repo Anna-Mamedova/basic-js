b6ae9b400777740cc7fbc811edf8c17609e905e9
Anna-Mamedova <mamedi289@gmail.com>
2023-04-14 18:25:45 +0300

    feat: add solutions for sum-digits.js and sort-by-height.js

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let newArr = arr.filter(i => i !== -1).sort((a, b) => a - b)
+  return arr.map(el => {
+    if(el === -1){
+      return el
+    } else return newArr.shift()
+  })
 }
 
 module.exports = {
